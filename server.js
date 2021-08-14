9702dad33e6de0ee19bcc4a06d48b31bb2bd0cfb
Vinette Gutierrez <vinette.gutierrez@gmail.com>
2021-08-13 22:56:33 -0700

    Commiting changes to server and re deploying to heroku

--- server.js
+++ server.js
@@ -15,12 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .catch((err) => console.log(err));
 
 // routes
 app.use(require("./routes/api.js"));
